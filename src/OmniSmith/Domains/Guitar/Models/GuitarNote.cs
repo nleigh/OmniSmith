e4218cbba063dbc28c39be6d47bb1e54a91a209a
Nathan Leigh <nleigh11@hotmail.com>
2026-04-12 17:43:05 +0100

    feat: implement guitar domain models and unit test infrastructure

--- src/OmniSmith/Domains/Guitar/Models/GuitarNote.cs
+++ src/OmniSmith/Domains/Guitar/Models/GuitarNote.cs
@@ -6,4 +6,7 @@ public class GuitarNote
     public int String { get; set; }
     public int Fret { get; set; }
     public float Duration { get; set; }
+    public NoteTechnique Techniques { get; set; }
+    public float BendValue { get; set; }
+    public int SlideTo { get; set; }
 }
